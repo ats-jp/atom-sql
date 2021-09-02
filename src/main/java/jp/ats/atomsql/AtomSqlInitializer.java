4124ec1803bd30cf61e09ad23a66e28b9f251de7
ats.tchiba <ats.t.chiba@gmail.com>
2021-09-03 08:49:11 +0900

    update

--- src/main/java/jp/ats/atomsql/AtomSqlInitializer.java
+++ src/main/java/jp/ats/atomsql/AtomSqlInitializer.java
@@ -7,60 +7,58 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionCustomizer;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * @author 千葉 哲嗣
  */
-@Configuration
-public class AtomSqlInitializer {
+public class AtomSqlInitializer implements ApplicationContextInitializer<AnnotationConfigApplicationContext> {
 
-	/**
-	 * @return SmartInitializingSingleton
-	 */
-	@Bean
-	public SmartInitializingSingleton start() {
-		return () -> {
-			List<Class<?>> classes;
+	@Override
+	public void initialize(AnnotationConfigApplicationContext context) {
+		List<Class<?>> classes;
+		try {
+			classes = loadProxyClasses();
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
 
-			try {
-				classes = loadProxyClasses();
-			} catch (IOException e) {
-				throw new UncheckedIOException(e);
-			}
+		var names = Configure.instance.jdbcTemplateNames();
 
-			try (var context = new AnnotationConfigApplicationContext()) {
-				var names = Configure.instance.jdbcTemplateNames();
-
-				Arrays.stream(names).forEach(name -> {
-					context.registerBean(name, AtomSql.class, () -> {
-						return new AtomSql(context.getBean(name, JdbcTemplate.class));
-					}, bd -> {
-						bd.setScope(BeanDefinition.SCOPE_SINGLETON);
-						bd.setLazyInit(true);
-					});
-
-					classes.forEach(c -> {
-						@SuppressWarnings("unchecked")
-						var casted = (Class<Object>) c;
-						context.registerBean(name, casted, () -> {
-							var atomSql = new AtomSql(context.getBean(name, JdbcTemplate.class));
-							return atomSql.of(c);
-						}, bd -> {
-							bd.setScope(BeanDefinition.SCOPE_SINGLETON);
-							bd.setLazyInit(true);
-						});
-					});
-				});
-
-				context.refresh();
-			}
+		BeanDefinitionCustomizer customizer = bd -> {
+			bd.setScope(BeanDefinition.SCOPE_SINGLETON);
+			bd.setLazyInit(true);
+			bd.setAutowireCandidate(true);
 		};
+
+		Arrays.stream(names).forEach(n -> {
+			var name = n.isEmpty() ? null : n;
+			context.registerBean(name, AtomSql.class, () -> {
+				if (name == null) {
+					return new AtomSql(context.getBean(JdbcTemplate.class));
+				}
+
+				return new AtomSql(context.getBean(name, JdbcTemplate.class));
+			}, customizer);
+
+			classes.forEach(c -> {
+				@SuppressWarnings("unchecked")
+				var casted = (Class<Object>) c;
+				context.registerBean(name, casted, () -> {
+					if (name == null) {
+						var atomSql = new AtomSql(context.getBean(JdbcTemplate.class));
+						return atomSql.of(c);
+					}
+
+					var atomSql = new AtomSql(context.getBean(name, JdbcTemplate.class));
+					return atomSql.of(c);
+				}, customizer);
+			});
+		});
 	}
 
 	private static List<Class<?>> loadProxyClasses() throws IOException {
